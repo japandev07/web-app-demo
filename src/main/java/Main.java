0281dad4ca1becf504252083776392e373d76564
japandev07 <japan.dev07@gmail.com>
2016-09-29 19:47:33 +0530

    test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,6 +10,6 @@ public class Main extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
-    resp.getWriter().print("Hello from Java!");
+    resp.getWriter().print("Hello from wffweb!");
   }
 }
