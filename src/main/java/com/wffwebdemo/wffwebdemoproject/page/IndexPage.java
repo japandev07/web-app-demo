c9a4a9ce1d4357c3b26be04f55330c70db6327fc
japandev07 <japan.dev07@gmail.com>
2016-09-30 00:58:32 +0530

    test

--- src/main/java/com/wffwebdemo/wffwebdemoproject/page/IndexPage.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/page/IndexPage.java
@@ -1,5 +1,8 @@
 package com.wffwebdemo.wffwebdemoproject.page;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import com.webfirmframework.wffweb.server.page.BrowserPage;
 import com.webfirmframework.wffweb.tag.html.AbstractHtml;
 import com.wffwebdemo.wffwebdemoproject.page.layout.IndexPageLayout;
@@ -10,9 +13,15 @@ public class IndexPage extends BrowserPage {
 
     @Override
     public String webSocketUrl() {
-        return "wss://wffweb.herokuapp.com/wffwebdemoproject/ws-for-index-page";
+        try {
+            return "wss://"+InetAddress.getLocalHost().getHostAddress()+"/wffwebdemoproject/ws-for-index-page";
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
+    
     @Override
     public AbstractHtml render() {
 
