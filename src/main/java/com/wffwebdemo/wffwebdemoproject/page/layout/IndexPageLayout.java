7c5bba12564f49bd82628df454a26f71068a99e4
japandev07 <japan.dev07@gmail.com>
2016-09-30 14:44:05 +0530

    Added server log link

--- src/main/java/com/wffwebdemo/wffwebdemoproject/page/layout/IndexPageLayout.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/page/layout/IndexPageLayout.java
@@ -3,9 +3,13 @@ package com.wffwebdemo.wffwebdemoproject.page.layout;
 import javax.servlet.http.HttpSession;
 
 import com.webfirmframework.wffweb.tag.html.Body;
+import com.webfirmframework.wffweb.tag.html.Br;
 import com.webfirmframework.wffweb.tag.html.Html;
 import com.webfirmframework.wffweb.tag.html.TitleTag;
+import com.webfirmframework.wffweb.tag.html.attribute.Href;
+import com.webfirmframework.wffweb.tag.html.attribute.Target;
 import com.webfirmframework.wffweb.tag.html.attribute.global.Style;
+import com.webfirmframework.wffweb.tag.html.links.A;
 import com.webfirmframework.wffweb.tag.html.metainfo.Head;
 import com.webfirmframework.wffweb.tag.htmlwff.NoTag;
 import com.wffwebdemo.wffwebdemoproject.page.model.DocumentModel;
@@ -42,6 +46,14 @@ public class IndexPageLayout extends Html {
         new Body(this, new Style("background:lightgray")) {
 
             {
+                new A(this, new Href("server-log"), new Target(Target.BLANK)) {
+                    {
+                        new NoTag(this, "view server log");
+                    }
+                };
+                
+                new Br(this);
+                
                 DocumentModel documentModel = new DocumentModel();
                 documentModel.setBody(this);
                 documentModel.setPageTitle(pageTitle);
