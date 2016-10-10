8757d51c19a5bf0a7683dcdd866fb446013364db
japandev07 <japan.dev07@gmail.com>
2016-10-10 23:53:40 +0530

    Url pattern improvement

--- src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,7 +16,7 @@ import com.wffwebdemo.wffwebdemoproject.page.IndexPage;
 /**
  * Servlet implementation class HomePageServlet
  */
-@WebServlet(urlPatterns = {"/index"})
+//@WebServlet(urlPatterns = {"/index"})
 public class IndexPageServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
