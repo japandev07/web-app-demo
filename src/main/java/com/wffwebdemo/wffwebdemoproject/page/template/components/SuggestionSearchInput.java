710e44a0aa551b03be739b47b031857189a12f93
japandev07 <japan.dev07@gmail.com>
2016-12-02 18:15:03 +0530

    Minor improvement

--- src/main/java/com/wffwebdemo/wffwebdemoproject/page/template/components/SuggestionSearchInput.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/page/template/components/SuggestionSearchInput.java
@@ -41,7 +41,9 @@ public class SuggestionSearchInput extends Div implements ServerAsyncMethod {
 
         dataListId = UUID.randomUUID().toString();
         
-        new Input(this, new Placeholder("type Alice") , new List(dataListId), new OnKeyUp("return (event.key.length == 1);", this, "return {fieldValue:source.value};", null));
+        new Input(this, new Placeholder("type Alice") , 
+                new List(dataListId), 
+                new OnKeyUp("return (event.key.length == 1 || event.key == 'Backspace');", this, "return {fieldValue:source.value};", null));
         
         
         dataList = new DataList(this,
