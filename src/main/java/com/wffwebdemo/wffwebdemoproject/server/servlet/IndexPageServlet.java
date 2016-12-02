5062ba941b43f8fb1479aa792f0859a291157f10
japandev07 <japan.dev07@gmail.com>
2016-12-02 13:19:47 +0530

    Fetching locale from header

--- src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
@@ -2,6 +2,9 @@ package com.wffwebdemo.wffwebdemoproject.server.servlet;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -16,9 +19,13 @@ import com.wffwebdemo.wffwebdemoproject.page.IndexPage;
 /**
  * Servlet implementation class HomePageServlet
  */
-//@WebServlet(urlPatterns = {"/index"})
+// @WebServlet(urlPatterns = {"/index"})
 public class IndexPageServlet extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
+
+    private static Logger LOGGER = Logger
+            .getLogger(IndexPageServlet.class.getName());
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -35,6 +42,27 @@ public class IndexPageServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html;charset=utf-8");
+
+        String localeFromHeader = request.getHeader("X-Wff-Locale");
+
+        Locale locale = request.getLocale();
+
+        if (localeFromHeader != null) {
+            try {
+                String[] langCountry = localeFromHeader.split("_");
+                if (langCountry.length == 2) {
+                    locale = new Locale(langCountry[0], langCountry[1]);
+                } else if (langCountry.length == 1) {
+                    locale = new Locale(langCountry[0]);
+                } else {
+                    throw new RuntimeException("langCountry.length is not 2 and 1");
+                }
+                LOGGER.info("X-Wff-Locale " + localeFromHeader);
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE,
+                        "Exception while converting X-Wff-Locale", e);
+            }
+        }
 
         try (OutputStream os = response.getOutputStream();) {
 
@@ -57,7 +85,7 @@ public class IndexPageServlet extends HttpServlet {
             }
 
             if (browserPage == null) {
-                browserPage = new IndexPage(session, request.getLocale());
+                browserPage = new IndexPage(session, locale);
                 BrowserPageContext.INSTANCE.addBrowserPage(session.getId(),
                         browserPage);
                 session.setAttribute("indexPageInstanceId",
