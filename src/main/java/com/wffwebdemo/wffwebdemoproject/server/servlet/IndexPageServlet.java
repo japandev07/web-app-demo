9099b34045157f7f833b2b85c99bca90596df691
japandev07 <japan.dev07@gmail.com>
2017-09-18 13:38:22 +0530

    Incorrect page content fix

--- src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
+++ src/main/java/com/wffwebdemo/wffwebdemoproject/server/servlet/IndexPageServlet.java
@@ -2,9 +2,11 @@ package com.wffwebdemo.wffwebdemoproject.server.servlet;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.Locale;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,20 +14,35 @@ import javax.servlet.http.HttpSession;
 
 import com.webfirmframework.wffweb.server.page.BrowserPage;
 import com.webfirmframework.wffweb.server.page.BrowserPageContext;
-import com.wffwebdemo.wffwebdemoproject.page.ServerLogPage;
+import com.wffwebdemo.wffwebdemoproject.page.IndexPage;
+import com.webfirmframework.wffweb.tag.html.attribute.core.AttributeRegistry;
+import com.webfirmframework.wffweb.tag.html.core.TagRegistry;
 
 /**
  * Servlet implementation class HomePageServlet
  */
-@WebServlet({"/server-log/*"})
-public class ServerLogPageServlet extends HttpServlet {
+// @WebServlet(urlPatterns = {"/index"})
+public class IndexPageServlet extends HttpServlet {
+
     private static final long serialVersionUID = 1L;
+
+    private static Logger LOGGER = Logger
+            .getLogger(IndexPageServlet.class.getName());
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServerLogPageServlet() {
+    public IndexPageServlet() {
         super();
+    }
+    
+    @Override
+    public void init() throws ServletException {
+    	super.init();
+    	
+    	TagRegistry.getTagClassNameByTagName();
+    	AttributeRegistry.getAttributeClassNameByAttributeName();
+    	LOGGER.info("Loaded all wffweb classes");
     }
 
     /**
@@ -36,24 +53,34 @@ public class ServerLogPageServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html;charset=utf-8");
-        
-        // all request other than server-log/realtime should redirect to server-log/realtime
-        // but server-log/realtime should not be used as a direct link in any wffweb single page.
-        // in a wffweb single page use server-log links to avoid unwanted removal of browserPage from its context
-        if (request.getRequestURI() == null 
-                || !request.getRequestURI().endsWith("server-log/realtime")) {
-            
-            response.sendRedirect("server-log/realtime");
-            return;
+
+        String localeFromHeader = request.getHeader("X-Wff-Locale");
+
+        Locale locale = request.getLocale();
+
+        if (localeFromHeader != null) {
+            try {
+                String[] langCountry = localeFromHeader.split("_");
+                if (langCountry.length == 2) {
+                    locale = new Locale(langCountry[0], langCountry[1]);
+                } else if (langCountry.length == 1) {
+                    locale = new Locale(langCountry[0]);
+                } else {
+                    throw new RuntimeException("langCountry.length is not 2 and 1");
+                }
+                LOGGER.info("X-Wff-Locale " + localeFromHeader);
+            } catch (Exception e) {
+                LOGGER.log(Level.SEVERE,
+                        "Exception while converting X-Wff-Locale", e);
+            }
         }
-        
 
         try (OutputStream os = response.getOutputStream();) {
 
             HttpSession session = request.getSession();
 
             String instanceId = (String) session
-                    .getAttribute("serverLogPageInstanceId");
+                    .getAttribute("indexPageInstanceId");
 
             BrowserPage browserPage = null;
             if (instanceId != null) {
@@ -69,10 +96,10 @@ public class ServerLogPageServlet extends HttpServlet {
             }
 
             if (browserPage == null) {
-                browserPage = new ServerLogPage();
+                browserPage = new IndexPage(session, locale);
                 BrowserPageContext.INSTANCE.addBrowserPage(session.getId(),
                         browserPage);
-                session.setAttribute("serverLogPageInstanceId",
+                session.setAttribute("indexPageInstanceId",
                         browserPage.getInstanceId());
             }
 
